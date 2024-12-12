3bae1cf1f9fd010fb49b0ae6573972931df67e93
thienhdbse170185 <thienhdbse170185@fpt.edu.vn>
2024-12-12 09:41:47 +0700

    Implement HealthyApiClient for symptom creation and add HealthyRepository for API interaction

--- lib/repository/healthy/healthy_api_client.dart
+++ lib/repository/healthy/healthy_api_client.dart
@@ -1,43 +1,41 @@
-import 'package:data_layer/model/entity/task/next_task/next_task.dart';
-import 'package:data_layer/model/response/task/task_by_cage/tasks_by_cage_response.dart';
-import 'package:data_layer/repository/data_client_interface.dart';
+import 'dart:developer';
 
-class HealthyApiClient extends IDataClient {
-  @override
-  Future<bool> create(entity) {
-    // TODO: implement create
-    throw UnimplementedError();
+import 'package:data_layer/api_endpoints.dart';
+import 'package:data_layer/model/request/symptom/create_symptom/create_symptom_request.dart';
+import 'package:dio/dio.dart';
+
+class HealthyApiClient {
+  final Dio dio;
+  HealthyApiClient({required this.dio});
+  Future<bool> create(CreateSymptomRequest request) async {
+    try {
+      final response =
+          await dio.post(ApiEndpoints.postSymptom, data: request.toJson());
+      if (response.statusCode == 200) {
+        return true;
+      }
+      return false;
+    } on DioException catch (e) {
+      log(e.toString());
+      rethrow;
+    }
   }
 
-  @override
   Future<bool> delete(String id) {
     // TODO: implement delete
     throw UnimplementedError();
   }
 
-  Future<List<NextTask>> getNextTask(String userId) {
-    // TODO: implement getNextTask
-    throw UnimplementedError();
-  }
-
-  Future<TasksByCageResponse> getTasksByCageId(String cageId) {
-    // TODO: implement getTasksByCageId
-    throw UnimplementedError();
-  }
-
-  @override
   Future read(String id) {
     // TODO: implement read
     throw UnimplementedError();
   }
 
-  @override
   Future testConnect() {
     // TODO: implement testConnect
     throw UnimplementedError();
   }
 
-  @override
   Future update(entity) {
     // TODO: implement update
     throw UnimplementedError();
