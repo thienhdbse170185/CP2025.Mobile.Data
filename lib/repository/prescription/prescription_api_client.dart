e891425a2e16edeedbb4d4285731eebf99421c4c
thienhdbse170185 <thienhdbse170185@fpt.edu.vn>
2025-03-07 22:03:44 +0700

    Add UpdateStatusPrescriptionRequest model and update API client methods for prescription status management

--- lib/repository/prescription/prescription_api_client.dart
+++ lib/repository/prescription/prescription_api_client.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:data_layer/api_endpoints.dart';
 import 'package:data_layer/model/dto/prescription/prescription.dart';
+import 'package:data_layer/model/request/prescription/update_status_prescription_request.dart';
 import 'package:dio/dio.dart';
 
 class PrescriptionApiClient {
@@ -10,16 +11,58 @@ class PrescriptionApiClient {
 
   Future<PrescriptionDto> getPrescriptionById(String prescriptionId) async {
     try {
+      log('[PRESCRIPTION_API_CLIENT] Lấy thông tin đơn thuốc theo id: $prescriptionId');
       final response = await dio
           .get('${ApiEndpoints.prescription}/$prescriptionId/prescription');
       if (response.statusCode == 200) {
+        log('[PRESCRIPTION_API_CLIENT] Lấy thông tin đơn thuốc thành công!');
         return PrescriptionDto.fromJson(response.data['result']);
       } else {
         log(response.data.toString());
         throw Exception('Failed to fetch prescription');
       }
     } on DioException catch (e) {
+      log('[PRESCRIPTION_API_CLIENT] Lỗi khi lấy thông tin đơn thuốc: ${e.message}');
       log(e.response!.data.toString());
+      rethrow;
+    }
+  }
+
+  Future<bool> updatePrescriptionStatus(
+      {required String prescriptionId,
+      required UpdateStatusPrescriptionRequest request}) async {
+    try {
+      final response = await dio.put(
+          '${ApiEndpoints.prescription}/$prescriptionId/status',
+          data: request.toJson());
+      if (response.statusCode == 200) {
+        log('[PRESCRIPTION_API_CLIENT] Cập nhật trạng thái đơn thuốc thành công!');
+        return true;
+      } else {
+        log(response.data.toString());
+        throw Exception('Failed to update prescription status');
+      }
+    } on DioException catch (e) {
+      log('[PRESCRIPTION_API_CLIENT] Lỗi khi cập nhật trạng thái đơn thuốc: ${e.message}');
+      log(e.toString());
+      rethrow;
+    }
+  }
+
+  Future<bool> checkPrescriptionLastSession(
+      {required String prescriptionId}) async {
+    try {
+      final response = await dio
+          .get('${ApiEndpoints.prescription}/$prescriptionId/is-last-session');
+      if (response.statusCode == 200) {
+        return response.data['result'];
+      } else {
+        log(response.data.toString());
+        throw Exception('Failed to check last session');
+      }
+    } on DioException catch (e) {
+      log('[PRESCRIPTION_API_CLIENT] Lỗi khi kiểm tra phiên cuối cùng: ${e.message}');
+      log(e.toString());
       rethrow;
     }
   }
--- lib/repository/prescription/prescription_repository.dart
+++ lib/repository/prescription/prescription_repository.dart
@@ -1,4 +1,5 @@
 import 'package:data_layer/model/dto/prescription/prescription.dart';
+import 'package:data_layer/model/request/prescription/update_status_prescription_request.dart';
 import 'package:data_layer/repository/prescription/prescription_api_client.dart';
 import 'package:dio/dio.dart';
 
@@ -9,6 +10,27 @@ class PrescriptionRepository {
   Future<PrescriptionDto> getPrescriptionById(String prescriptionId) async {
     try {
       return await apiClient.getPrescriptionById(prescriptionId);
+    } on DioException {
+      rethrow;
+    }
+  }
+
+  Future<bool> updatePrescriptionStatus(
+      {required String prescriptionId,
+      required UpdateStatusPrescriptionRequest request}) async {
+    try {
+      return await apiClient.updatePrescriptionStatus(
+          prescriptionId: prescriptionId, request: request);
+    } on DioException {
+      rethrow;
+    }
+  }
+
+  Future<bool> checkPrescriptionLastSession(
+      {required String prescriptionId}) async {
+    try {
+      return await apiClient.checkPrescriptionLastSession(
+          prescriptionId: prescriptionId);
     } on DioException {
       rethrow;
     }
